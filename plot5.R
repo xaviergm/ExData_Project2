b926300038a4280f12ea9e418024a689ab41baf2
xaviergm <xavier.gutierrez@roche.com>
2015-05-11 15:33:01 +0200

    question 5 solved through plot5.png and plot5.R

---
+++ plot5.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(ggplot2)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+motorRelatedSCC <- SCC$SCC[grep("Motor",SCC$SCC.Level.Three)]
+motorEmissionsBaltimoreSummarized <- NEI %>%
+        filter(SCC %in% motorRelatedSCC, fips == 24510) %>%
+        group_by(year) %>% summarize(Emissions = sum(Emissions))
+png("plot5.png")
+plot5 <- qplot(year,Emissions,data=motorEmissionsBaltimoreSummarized, 
+               geom=c("point","smooth"),method="lm")
+print(plot5)
+dev.off()
